3c4bf5f515ca29dbbf1c8a68339ad56371f44051
Alex <supamumas@gmail.com>
2020-09-12 00:23:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
 
 let friends = ["Paco","Fer","John","Fran"]
-body.style.color = '#4682B4'
+document.body.style.color = '#4682B4'
 let friend = document.createElement('div');
 friend.className = "friend";
 document.body.appendChild(friend);
